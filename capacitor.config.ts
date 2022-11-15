a168ae22e968dbd4bbd9b89416cfdfba2e66555e
Michele ATEUTCHIA <micheleateutchia@gmail.com>
2022-11-15 17:45:05 +0100

    open my project on android studio

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,7 @@ const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'shoppingCart',
   webDir: 'www',
+  //CAPACITOR_ANDROID_STUDIO_PATH:'android-studio/bin/studio.sh',
   bundledWebRuntime: false
 };
 
